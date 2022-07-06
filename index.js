132312df60ce5844e12e12ecbf4c7a84ee1820f1
AleksandrKhv <sidby83@gmail.com>
2022-07-06 14:14:36 +0200

    fix cors

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ mongoose
   .catch((err) => console.log('DB error', err))
 
 const app = express()
+const cors = require('cors')
 
 const storage = multer.diskStorage({
   destination: (_, __, cb) => {
